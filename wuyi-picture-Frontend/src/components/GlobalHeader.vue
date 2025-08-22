7252e42c02c3e92df4156d3a6d2c5dc25c785735
wuyinai <19535667165@163.com>
2025-08-22 17:21:30 +0800

    前端完成： 前端初始化 前端登录注册 前端用户管理 前端图片关系 前端主页

---
+++ wuyi-picture-Frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <router-link to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="logo" />
+            <div class="title">AI飞云图库</div>
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <ASpace>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </ASpace>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed, h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { type MenuProps, message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '@/stores/userLoginUserStore'
+import { userLogoutUsingPost } from '@/api/userController'
+
+const loginUserStore = useLoginUserStore()
+
+const router = useRouter()
+// 当前要高亮的菜单项
+const current = ref<string[]>([])
+
+// 菜单列表
+const originItems = [
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理',
+  },
+  {
+    key: '/admin/pictureManage',
+    label: '图片管理',
+    title: '图片管理',
+  },
+  {
+    key: '/add_picture',
+    label: '创建图片',
+    title: '创建图片',
+  }
+]
+
+// 过滤菜单项
+const filterMenus = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    if (menu.key.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== "admin") {
+        return false
+      }
+    }
+    return true
+  })
+}
+
+// 展示在菜单的路由数组
+const items = computed<MenuProps['items']>(() => filterMenus(originItems))
+
+// 监听路由变化，更新高亮菜单项
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+
+// 路由跳转事件
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key,
+  })
+}
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  console.log(res)
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
+</script>
+
+<style scoped>
+#globalHeader .title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ wuyi-picture-Frontend/src/stores/userLoginUserStore.ts
@@ -0,0 +1,32 @@
+import {ref} from 'vue'
+import {defineStore} from 'pinia'
+import { getLoginUserUsingGet } from '@/api/userController'
+
+/**
+ * 存储登录用户信息的状态
+ */
+export const useLoginUserStore = defineStore('loginUser', () => {
+  const loginUser = ref<API.LoginUserVO>({
+    userName: '未登录',
+  })
+
+
+  async function fetchLoginUser() {
+    const res = await getLoginUserUsingGet()
+    if (res.data.code === 0 && res.data.data) {
+      loginUser.value = res.data.data
+    }
+  }
+
+
+  /**
+   * 设置登录用户
+   * @param newLoginUser
+   */
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
+  }
+
+  // 返回
+  return { loginUser, fetchLoginUser, setLoginUser }
+})
